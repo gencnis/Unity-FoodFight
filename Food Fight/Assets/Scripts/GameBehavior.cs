817c83406f94ac3f61e02f363d1f30676baf18a4
AlexWills37 <alexanderwills37@gmail.com>
2022-01-21 10:37:13 -0500

    Added GameBehavior and PlayerBehavior

---
+++ Food Fight/Assets/Scripts/GameBehavior.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Defines how the game will work
+/// </summary>
+public class GameBehavior : MonoBehaviour
+{
+    public int maxNumSnickers = 5;
+    public bool showWinScreen = false;
+
+    private int _snickersCollected;
+    /// <summary>
+    /// Access variable for the number of snickers bar the player has collected
+    /// </summary>
+    public int SnickersCollected
+    {
+        get { return _snickersCollected; }
+        set
+        {
+            _snickersCollected = value;
+            Debug.LogFormat("You've eaten {0} snickers bars!", _snickersCollected);
+
+            // If the user has collected all the snickers, the game is won
+            if(_snickersCollected >= maxNumSnickers)
+            {
+                showWinScreen = true;
+                Debug.Log("Congratulations! You have eaten all the snickers bars.");
+            }
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Food Fight/Assets/Scripts/PlayerBehavior.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Main script to define how the player behaves
+/// </summary>
+public class PlayerBehavior : MonoBehaviour
+{
+    public GameBehavior gameBehavior;
+    public Transform playerTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.gameBehavior = GameObject.Find("Game Manager").GetComponent<GameBehavior>();
+        this.playerTransform = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // This if block is just for testing purposes
+        if (Input.GetMouseButtonDown(0))
+        {
+            GainPoint();
+        }
+    }
+
+
+    /// <summary>
+    /// When the user eats a snickers bar, scale their size by 1 and add a point to the game manager
+    /// </summary>
+    private void GainPoint()
+    {
+        playerTransform.localScale = playerTransform.localScale + Vector3.one;
+        gameBehavior.SnickersCollected = gameBehavior.SnickersCollected + 1;    
+    }
+}
