3f7fedde5c7cce76b13d84ba8dd61ae4572a82ac
VladTsimoshchanka <78988772+VladTsimoshchanka@users.noreply.github.com>
2022-01-22 22:09:15 -0500

    Final push with GUI

--- Food Fight/Assets/Scripts/GameBehavior.cs
+++ Food Fight/Assets/Scripts/GameBehavior.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 /// <summary>
 /// Defines how the game will work
@@ -9,9 +10,9 @@ public class GameBehavior : MonoBehaviour
 {
     public int maxNumSnickers = 5;
     public bool showWinScreen = false;
-
+   public string labelText = "Devour 5 watermelons and win your freedom! Remember: the walls haunt you, but don't trust them!";
     private int _snickersCollected;
-
+    bool showGUI = false;
     public WallScript wall;
 
 
@@ -29,8 +30,14 @@ public class GameBehavior : MonoBehaviour
             // If the user has collected all the snickers, the game is won
             if(_snickersCollected >= maxNumSnickers)
             {
+                labelText = "You've eaten them all!";
                 showWinScreen = true;
                 Debug.Log("Congratulations! You have eaten all the snickers bars.");
+                Time.timeScale = 0f;
+            }
+            else
+            {
+                labelText = "Watermelon down, only " + (maxNumSnickers - _snickersCollected) + " more to go!";
             }
         }
     }
@@ -50,5 +57,37 @@ public class GameBehavior : MonoBehaviour
     public void PointCollected()
     {
         wall.HauntThePlayer();
+    }
+
+
+    void OnEnable()
+    {
+        showGUI = true;
+    }
+    void OnDisable()
+    {
+        showGUI = false;
+    }
+
+    //Update the GUI with the current state of the game
+    void OnGUI()
+    {
+        if (showGUI)
+        {
+            GUI.Box(new Rect(20, 50, 200, 30), "Watermelons harvested: " + _snickersCollected);
+            // 6
+            GUI.Label(new Rect(Screen.width / 2 - 100, Screen.height - 50, 300, 50), labelText);
+            if (showWinScreen)
+            {
+                if (GUI.Button(new Rect(Screen.width / 2 - 100,
+               Screen.height / 2 - 50, 200, 100), "YOU WON!"))
+                {
+                    // 3
+                    SceneManager.LoadScene(0);
+                    // 4
+                    Time.timeScale = 1.0f;
+                }
+            }
+        }
     }
 }
